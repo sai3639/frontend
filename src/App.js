9964ff491897f99a1e6edbfadf666fc0b489da37
sai3639 <saira3639@gmail.com>
2024-09-24 03:12:20 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Tweet from './components/Tweet';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Nav/>
+       <Switch>
+
+          <Route path="/" exact component={Home}/>
+          <Route path="/tweets" exact component={Tweet}/>
+
+         </Switch>
       </header>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Home() {
+    return(
+        <section>
+            <div>
+                <h1>welcome</h1>
+                <p>site created using node js and react</p>
+            </div>
+        </section>
+
+
+
+    );
+}
+
+export default Home; 
